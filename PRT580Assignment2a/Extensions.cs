d99bc05a90c40990677c37a4f342b89c9a18aab7
LargerSnowdevil <drac1223@hotmail.com>
2023-08-09 14:08:16 +0930

    Add project files.

---
+++ PRT580Assignment2a/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRT580Assignment2a
+{
+    public static class Extensions
+    {
+        public static List<int> ToList(this string csv)
+        {
+            var temp = csv.Split(',').ToList();
+            return temp.Select(int.Parse).ToList();
+        }
+
+        public static string ToCsv(this List<int> list)
+        {
+            return string.Join(",", list);
+        }
+    }
+}
---
+++ PRT580Assignment2a/Logic.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRT580Assignment2a
+{
+    public class Logic
+    {
+        public Logic() { }
+
+        public static (List<int> Primes, List<int> NonPrimes) SeperatePrimes(List<int> mixedNumbers)
+        {
+            return SplitPrimes(mixedNumbers);
+        }
+
+        public static List<int> FindPrimes(List<int> mixedNumbers)
+        {
+            return SplitPrimes(mixedNumbers).Primes;
+        }
+
+        public static List<int> FindComposites(List<int> mixedNumbers)
+        {
+            return SplitPrimes(mixedNumbers).NonPrimes;
+        }
+
+        public static List<int> Partition_P_NP(List<int> A)
+        {
+            var splitLists = SplitPrimes(A);
+            var response = new List<int>();
+            response.AddRange(splitLists.Primes);
+            response.AddRange(splitLists.NonPrimes);
+            return response;
+        }
+
+        private static (List<int> Primes, List<int> NonPrimes) SplitPrimes(List<int> mixedNumbers)
+        {
+            var primes = new List<int>();
+            var nonPrimes = new List<int>();
+
+            foreach(var number in mixedNumbers)
+            {
+                if (IsPrime(number))
+                {
+                    primes.Add(number);
+                }
+                else
+                {
+                    nonPrimes.Add(number);
+                }
+            }
+
+            return (Primes: primes, NonPrimes: nonPrimes);
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+
+            for (var i = 2; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return number == i;
+                }
+            }
+
+            throw new Exception("Number was not divisable by itself somehow.");
+        }
+    }
+}
